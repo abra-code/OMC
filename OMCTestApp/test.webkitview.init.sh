f8bdeca705fe8b6d0775f077de70f14ec06a063c
Tomasz Kukielka <tom-dev@abracode.com>
2020-01-04 00:20:29 -0800

    Add OMCWebKitView to the framework and add test code
    Rename deperecated OMCWebKit tests to Legacy and add new OMCWebKitView test
    Minor non-functional cleanups in tests

---
+++ OMCTestApp/test.webkitview.init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+"$OMC_OMC_SUPPORT_PATH/omc_dialog_control" "$OMC_NIB_DLG_GUID" 1 "file://$OMC_APP_BUNDLE_PATH/Contents/Resources/test.webkit.example.html"
+"$OMC_OMC_SUPPORT_PATH/omc_dialog_control" "$OMC_NIB_DLG_GUID" 2 "file://$OMC_APP_BUNDLE_PATH/Contents/Resources/test.webkit.example.html"
